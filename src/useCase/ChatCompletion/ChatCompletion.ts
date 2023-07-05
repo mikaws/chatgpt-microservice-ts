60dda6af0ba979d895b9f46d7372cc98bf89c6b8
Michael <michaelhenriqued@gmail.com>
2023-07-05 08:16:56 -0300

    feat: when chat not found create a new chat and append user message

--- src/useCase/ChatCompletion/ChatCompletion.ts
+++ src/useCase/ChatCompletion/ChatCompletion.ts
@@ -1,8 +1,11 @@
 import { ChatGateway } from "../../domain/gateway/ChatGateway";
 import { OpenAIApi } from "openai";
 import { Either, left, right } from "../../shared/either";
+import { Chat, TChatConfig } from "../../domain/entity/Chat";
+import { Model } from "../../domain/entity/Model";
+import { Message } from "../../domain/entity/Message";
 
-type ChatCompletionConfigInputDTO = {
+export type ChatCompletionConfigInputDTO = {
   model: string;
   modelMaxTokens: number;
   temperature: number;
@@ -15,14 +18,14 @@ type ChatCompletionConfigInputDTO = {
   initialSystemMessage: string;
 };
 
-type ChatCompletionInputDTO = {
+export type ChatCompletionInputDTO = {
   chatId: string;
   userId: string;
   userMessage: string;
   config: ChatCompletionConfigInputDTO;
 };
 
-type ChatCompletionOutputDTO = {
+export type ChatCompletionOutputDTO = {
   chatId: string;
   userId: string;
   content: string;
@@ -40,16 +43,78 @@ export class ChatCompletionUseCase {
   execute(
     input: ChatCompletionInputDTO
   ): Either<Error, ChatCompletionOutputDTO> {
-    const chatOrError = this.chatGateway.findChatById(input.chatId);
+    let chatOrError = this.chatGateway.findChatById(input.chatId);
+    let chat: Chat;
+    let err: Error;
+
     if (chatOrError.isLeft()) {
-      const error = chatOrError.value;
-      if (error.message == "chat not found") return left(chatOrError.value);
+      err = chatOrError.value;
+      if (err.message == "chat not found") {
+        const newChatOrError = this.createNewChat(input);
+        if (newChatOrError.isLeft()) {
+          err = newChatOrError.value;
+          return left(err);
+        }
+        chat = newChatOrError.value;
+      }
       return left(new Error("error fetching existing chat"));
+    } else {
+      chat = chatOrError.value;
+    }
+
+    const userMessageOrError = Message.create(
+      "user",
+      input.userMessage,
+      chat.config.model
+    );
+    if (userMessageOrError.isLeft()) {
+      return left(new Error("error creating user message"));
+    }
+    const userMessage = userMessageOrError.value;
+
+    const messageAddedOrError = this.addMessageOnChat(chat, userMessage);
+    if (messageAddedOrError.isLeft()) {
+      return left(new Error("error adding new message"));
     }
     return right({
       chatId: input.chatId,
       content: "mock",
       userId: input.userId,
     }); // content is mocked for now
+  }
+
+  addMessageOnChat(chat: Chat, userMessage: Message): Either<Error, Message> {
+    return chat.addMessage(userMessage);
+  }
+
+  createNewChat(input: ChatCompletionInputDTO): Either<Error, Chat> {
+    const model = new Model(input.config.model, input.config.modelMaxTokens);
+    const chatConfig: TChatConfig = {
+      temperature: input.config.temperature,
+      topP: input.config.topP,
+      n: input.config.n,
+      stop: input.config.stop,
+      maxTokens: input.config.maxTokens,
+      presencePenalty: input.config.presencePenalty,
+      frequencyPenalty: input.config.frequencyPenalty,
+      model,
+    };
+    const initialMessageOrError = Message.create(
+      "system",
+      input.config.initialSystemMessage,
+      model
+    );
+    if (initialMessageOrError.isLeft()) {
+      return left(new Error("error creating initial message"));
+    }
+    const initialMessage = initialMessageOrError.value;
+
+    const chatOrError = Chat.create(input.userId, initialMessage, chatConfig);
+    if (chatOrError.isLeft()) {
+      return left(new Error("error creating new chat"));
+    }
+    const chat = chatOrError.value;
+
+    return right(chat);
   }
 }
