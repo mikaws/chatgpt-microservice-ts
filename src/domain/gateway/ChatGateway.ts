03590cb47ee9d6ab3b0d5ad5037c936fbcad4466
Michael <michaelhenriqued@gmail.com>
2023-09-09 11:49:10 -0300

    feat: add more methods types in ChatGateway

--- src/domain/gateway/ChatGateway.ts
+++ src/domain/gateway/ChatGateway.ts
@@ -2,5 +2,7 @@ import { Either } from "../../shared/either";
 import { Chat } from "../entity/Chat";
 
 export type ChatGateway = {
+  createChat(chat: Chat): Either<Error, void>
   findChatById(chatId: string): Either<Error, Chat>;
+	saveChat(chat: Chat): Either<Error, void>
 };
