017bc474544c40d2f4febb32df70d39880125b44
Michael <michaelhenriqued@gmail.com>
2023-10-07 19:20:35 -0300

    fix(in-memory-chat-repository): createChat findIndex comparison

--- src/domain/repository/InMemoryChatRepository.ts
+++ src/domain/repository/InMemoryChatRepository.ts
@@ -6,20 +6,26 @@ export class InMemoryChatRepository implements ChatRepository {
   private chats: Chat[] = [];
 
   async createChat(chat: Chat): Promise<Either<Error, Chat>> {
-    const chatIndex = this.chats.findIndex((c) => c.id === c.id);
+    const chatIndex = this.chats.findIndex(
+      (chatFromRepository) => chatFromRepository.id === chat.id
+    );
     if (chatIndex !== -1) return left(new Error("chat already exists"));
     this.chats.push(chat);
     return right(chat);
   }
 
   async findChatById(chatId: string): Promise<Either<Error, Chat>> {
-    const chat = this.chats.find((chat) => chat.id === chatId);
+    const chat = this.chats.find(
+      (chatFromRepository) => chatFromRepository.id === chatId
+    );
     if (!chat) return left(new Error("chat not found"));
     return right(chat);
   }
 
   async saveChat(chat: Chat): Promise<Either<Error, Chat>> {
-    const chatIndex = this.chats.findIndex((c) => c.id === chat.id);
+    const chatIndex = this.chats.findIndex(
+      (chatFromRepository) => chatFromRepository.id === chat.id
+    );
     if (chatIndex === -1) return left(new Error("chat not found"));
     this.chats[chatIndex] = chat;
     return right(chat);
