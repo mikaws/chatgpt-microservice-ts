e8f8e88e2add90a469ff583cc6cba49509c69f60
Michael <michaelhenriqued@gmail.com>
2023-10-18 22:41:37 -0300

    feat(ChatCompletionController): now it receives chatConfig via dependency injection

--- src/presentation/controller/ChatCompletionController.ts
+++ src/presentation/controller/ChatCompletionController.ts
@@ -1,16 +1,28 @@
 import { Controller } from "../protocols/controller";
-import { ChatCompletionInputDTO } from "../../domain/usecase/ChatCompletionDTO";
+import {
+  ChatCompletionConfigInputDTO,
+  ChatCompletionInputDTO,
+} from "../../domain/usecase/ChatCompletionDTO";
 import { ChatCompletionUseCase } from "../../domain/usecase/ChatCompletionUseCase";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class ChatCompletionController implements Controller {
-  constructor(private chatCompletionUseCase: ChatCompletionUseCase) {}
+  constructor(
+    private chatCompletionUseCase: ChatCompletionUseCase,
+    private chatConfig: ChatCompletionConfigInputDTO
+  ) {}
 
   public async handler(req: HttpRequest): Promise<HttpResponse> {
     try {
-      const dto = req.body as ChatCompletionInputDTO;
+      const { chatId, userId, userMessage } = req.body;
+      const dto: ChatCompletionInputDTO = {
+        chatId,
+        config: this.chatConfig,
+        userId,
+        userMessage,
+      };
       const resultOrError = await this.chatCompletionUseCase.execute(dto);
-      if(resultOrError.isLeft()) {
+      if (resultOrError.isLeft()) {
         const error = resultOrError.value;
         throw error;
       }
