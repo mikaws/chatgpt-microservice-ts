27e2a9035d788a307c5d655b0ccfa80e5c8ae123
Michael <michaelhenriqued@gmail.com>
2023-10-18 22:39:18 -0300

    test(PostgreSQLRepository): ensure all methods in repository are correct

--- src/infra/repository/PostgreSQLRepository.spec.ts
+++ src/infra/repository/PostgreSQLRepository.spec.ts
@@ -11,7 +11,6 @@ describe("PostgreSQLRepository", () => {
     sut: PostgreSQLRepository;
     fakeChat: Chat;
     fakeModel: Model;
-    fakeMessage: Message;
     chatConfigInput: ChatCompletionConfigInputDTO;
   };
   beforeEach(async () => {
@@ -31,14 +30,14 @@ describe("PostgreSQLRepository", () => {
       };
       const fakeModel = Model.create(
         chatConfigInput.model,
-        chatConfigInput.maxTokens
+        chatConfigInput.modelMaxTokens
       ).value as Model;
-      const fakeMessage = Message.create(
+      const fakeInitialMessage = Message.create(
         "system",
         chatConfigInput.initialSystemMessage,
         fakeModel
       ).value as Message;
-      const fakeChat = Chat.create("uuid", fakeMessage, {
+      const fakeChat = Chat.create("uuid", fakeInitialMessage, {
         frequencyPenalty: chatConfigInput.frequencyPenalty,
         maxTokens: chatConfigInput.maxTokens,
         model: fakeModel,
@@ -48,7 +47,7 @@ describe("PostgreSQLRepository", () => {
         temperature: chatConfigInput.temperature,
         topP: chatConfigInput.temperature,
       }).value as Chat;
-      return { sut, fakeChat, fakeModel, fakeMessage, chatConfigInput };
+      return { sut, fakeChat, chatConfigInput, fakeModel };
     };
     await postgreSQLDatabase.pool.query(`
       CREATE TEMPORARY TABLE IF NOT EXISTS chats (LIKE chats INCLUDING ALL);
@@ -79,15 +78,12 @@ describe("PostgreSQLRepository", () => {
     const { sut, fakeChat } = makeSut();
     jest
       .spyOn(postgreSQLDatabase, "createChat")
-      .mockResolvedValue(left(new Error("message: fatal err; stack: line 1")));
+      .mockResolvedValue(left(new Error("message: fatal err in 'createChat'; stack: line 1")));
     const error = await sut.createChat(fakeChat);
     expect(error.isLeft()).toBeTruthy();
     expect(error).toEqual(
       left(
-        new Error(
-          "error creating chat: " +
-            new Error("message: fatal err; stack: line 1")
-        )
+        new Error("error creating chat: message: fatal err in 'createChat'; stack: line 1")
       )
     );
   });
@@ -95,22 +91,173 @@ describe("PostgreSQLRepository", () => {
     const { sut, fakeChat } = makeSut();
     jest
       .spyOn(postgreSQLDatabase, "addMessage")
-      .mockResolvedValue(left(new Error("message: fatal err; stack: line 2")));
+      .mockResolvedValue(left(new Error("message: fatal err in 'addMessage'; stack: line 2")));
     const error = await sut.createChat(fakeChat);
     expect(error.isLeft()).toBeTruthy();
     expect(error).toEqual(
       left(
         new Error(
-          "error saving message when creating chat: " +
-            new Error("message: fatal err; stack: line 2")
+          "error saving message when creating chat: message: fatal err in 'addMessage'; stack: line 2"
         )
       )
     );
   });
-  it("should return chat when creating a chat in createChat method", async () => {
+  it("should return right with chat when creating a chat in createChat method", async () => {
     const { sut, fakeChat } = makeSut();
     const chatOrError = await sut.createChat(fakeChat);
     expect(chatOrError.isRight()).toBeTruthy();
     expect(chatOrError).toEqual(right(fakeChat));
+  });
+  it("should return left when error occurs saving a chat in updateChat method", async () => {
+    const { sut, fakeChat } = makeSut();
+    jest
+      .spyOn(postgreSQLDatabase, "updateChat")
+      .mockResolvedValue(left(new Error("message: fatal err in 'updateChat'; stack: line 1")));
+    const error = await sut.updateChat(fakeChat);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error("error saving chat: message: fatal err in 'updateChat'; stack: line 1")
+      )
+    );
+  });
+  it("should return left when error occurs deleting messages in updateChat method", async () => {
+    const { sut, fakeChat } = makeSut();
+    jest
+      .spyOn(postgreSQLDatabase, "deleteChatMessages")
+      .mockResolvedValue(left(new Error("message: fatal err in 'deleteChatMessages'; stack: line 1")));
+    const error = await sut.updateChat(fakeChat);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error(
+          "error deleting chat messages: message: fatal err in 'deleteChatMessages'; stack: line 1"
+        )
+      )
+    );
+  });
+  it("should return left when error occurs deleting erased messages in updateChat method", async () => {
+    const { sut, fakeChat } = makeSut();
+    jest
+      .spyOn(postgreSQLDatabase, "deleteErasedChatMessages")
+      .mockResolvedValue(left(new Error("message: fatal err in 'deleteErasedChatMessages'; stack: line 1")));
+    const error = await sut.updateChat(fakeChat);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error(
+          "error deleting erased chat messages: message: fatal err in 'deleteErasedChatMessages'; stack: line 1"
+        )
+      )
+    );
+  });
+  it("should return left when error occurs saving messages in updateChat method", async () => {
+    const { sut, fakeChat, fakeModel } = makeSut();
+    jest
+      .spyOn(postgreSQLDatabase, "addMessage")
+      .mockResolvedValue(left(new Error("message: fatal err in 'addMessage'; stack: line 1")));
+    const userMessage = Message.create("user", "The sky is blue?", fakeModel)
+      .value as Message;
+    fakeChat.addMessage(userMessage);
+    const error = await sut.updateChat(fakeChat);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error(
+          "error adding messages to saved chat: message: fatal err in 'addMessage'; stack: line 1"
+        )
+      )
+    );
+  });
+  it("should return left when error occurs saving erased messages in updateChat method", async () => {
+    const { sut, fakeChat, fakeModel } = makeSut();
+    jest
+      .spyOn(postgreSQLDatabase, "addMessage")
+      .mockResolvedValue(
+        left(new Error("message: fatal err in 'addMessage'; stack: line 1"))
+      );
+    const erasedMessage = Message.create("user", "Message deleted", fakeModel)
+      .value as Message;
+    fakeChat.addErasedMessage(erasedMessage);
+    const error = await sut.updateChat(fakeChat);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error(
+          "error adding erased messages to saved chat: message: fatal err in 'addMessage'; stack: line 1"
+        )
+      )
+    );
+  });
+  it("should return left when error occurs findind a chat in findChatById method", async () => {
+    const { sut, fakeChat } = makeSut();
+    jest
+      .spyOn(postgreSQLDatabase, "findChatById")
+      .mockResolvedValue(left(new Error("message: fatal err in 'findChatById'; stack: line 1")));
+    const error = await sut.findChatById(fakeChat.id);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error("error finding chat: message: fatal err in 'findChatById'; stack: line 1")
+      )
+    );
+  });
+  it("should return left when error occurs chat not found in findChatById method", async () => {
+    const { sut, fakeChat } = makeSut();
+    const error = await sut.findChatById(fakeChat.id);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(left(new Error("chat not found")));
+  });
+  it("should return left when error occurs findind messages in findMessagesByChatID method", async () => {
+    const { sut, fakeChat } = makeSut();
+    await sut.createChat(fakeChat);
+    jest
+      .spyOn(postgreSQLDatabase, "findMessagesByChatId")
+      .mockResolvedValue(left(new Error("message: fatal err in 'findMessagesByChatId'; stack: line 1")));
+    const error = await sut.findChatById(fakeChat.id);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error(
+          "error finding messages: message: fatal err in 'findMessagesByChatId'; stack: line 1"
+        )
+      )
+    );
+  });
+  it("should return left when error occurs findind erased messages in findErasedMessagesByChatID method", async () => {
+    const { sut, fakeChat } = makeSut();
+    await sut.createChat(fakeChat);
+    jest
+      .spyOn(postgreSQLDatabase, "findErasedMessagesByChatId")
+      .mockResolvedValue(left(new Error("message: fatal err in 'findErasedMessagesByChatId'; stack: line 1")));
+    const error = await sut.findChatById(fakeChat.id);
+    expect(error.isLeft()).toBeTruthy();
+    expect(error).toEqual(
+      left(
+        new Error(
+          "error finding erased messages: message: fatal err in 'findErasedMessagesByChatId'; stack: line 1"
+        )
+      )
+    );
+  });
+  it("should return right with chat when creating a chat in findChatById method", async () => {
+    const { sut, fakeChat, fakeModel } = makeSut();
+    await sut.createChat(fakeChat);
+    const userMessage = Message.create("user", "The sky is blue?", fakeModel)
+      .value as Message;
+    const message = Message.create(
+      "assistant",
+      "Yes, the sky is blue.",
+      fakeModel
+    ).value as Message;
+    const erasedMessage = Message.create("user", "Message deleted", fakeModel)
+      .value as Message;
+    fakeChat.addMessage(userMessage);
+    fakeChat.addMessage(message);
+    fakeChat.addErasedMessage(erasedMessage);
+    await sut.updateChat(fakeChat);
+    const chat = await sut.findChatById(fakeChat.id);
+    expect(chat.isRight()).toBeTruthy();
+    expect(chat).toEqual(right(fakeChat));
   });
 });
