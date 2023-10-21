986d3c4599fa24a3ff541f923f758d961b5cf552
Michael <michaelhenriqued@gmail.com>
2023-10-21 10:06:04 -0300

    test(main): cors middleware

---
+++ src/main/config/middlewares/cors.test.ts
@@ -0,0 +1,21 @@
+import { Application } from "../../Application";
+import type { Express } from "express";
+import request from "supertest";
+
+let app: Express;
+
+describe("cors", () => {
+  beforeAll(async () => {
+    app = await Application.setup();
+  });
+  it("should enable cors", async () => {
+    app.get("/test-cors", (req, res) => {
+      console.log("teste ", req.headers);
+    });
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  });
+});
